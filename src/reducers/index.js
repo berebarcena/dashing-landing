de2e586d8a85a8f62268ed5a74709f221a5aca4f
berebarcena <berebarcenarm@gmail.com>
2017-08-08 16:51:52 +0200

    Removed unnecessary code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 
 // Import your reducers here
 import navigation from './navigation';
-import projects from './projects';
 
 const reducer = combineReducers({
   // Add your reducers to the list
   navigation,
-  projects,
 });
 
 export default reducer;
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 import Layout from '../containers/Layout';
 import Home from '../containers/Home';
-import Projects from '../containers/Projects';
-import Project from '../containers/Project';
-import About from '../containers/About';
 import NotFound from '../containers/NotFound';
 
 import {
@@ -16,19 +13,16 @@ import {
 export default class Router extends Component {
   render() {
     // NOTE: routerProps need to be removed once the gh-pages site url is changed to
-    // http://berebarcena.com
+    // http://dashing.dk
     const routerProps = {};
     if (process.env.NODE_ENV === 'production') {
-      routerProps.basename = '/berebarcena.com';
+      routerProps.basename = '/dashing-landing';
     }
     return (
       <BrowserRouter {...routerProps}>
         <Layout>
           <Switch>
             <Route exact path="/" component={Home}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/projects" component={Projects}/>
-            <Route path="/projects/:projectId" component={Project} />
             <Route component={NotFound} />
           </Switch>
         </Layout>

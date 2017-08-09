2a876c2530e6432045e99b4f20f93267623ac541
Marcos Mellado <marcos@mellados.com>
2017-08-09 21:18:18 +0200

    Fixed lint errors

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -49,14 +49,14 @@ export default class EmailForm extends Component {
           body:`${this.formField}=${emailValue}`
         })
         .then(res => res.text())
-        .then(res => {
+        .then(() => {
           // If no errors, clear loader and show thank you
           this.setState({
             isSubmitting: false,
             isSubmitted: true
           });
         })
-        .catch(err => {
+        .catch(() => {
           // Since there will be CORS error, we will always fall here. This will still write to the
           // google sheet so it doesn't matter. Clear loader and show thank you
           this.setState({
@@ -75,7 +75,7 @@ export default class EmailForm extends Component {
 
   // Validates the email address
   validateEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
 

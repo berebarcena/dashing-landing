f0468d4c3a011195848259a1c89f4974ea7a8bf9
Marcos Mellado <marcos@mellados.com>
2017-08-09 21:30:40 +0200

    Updated loader

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -100,7 +100,11 @@ export default class EmailForm extends Component {
     const { isSubmitting, emailValue, isInvalidEmail } = this.state;
     return isSubmitting
       ?
-        <div className="loader">Submitting....</div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <path fill="none" d="M24.3,30C11.4,30,5,43.3,5,50s6.4,20,19.3,20c19.3,0,32.1-40,51.4-40 C88.6,30,95,43.3,95,50s-6.4,20-19.3,20C56.4,70,43.6,30,24.3,30z" stroke="#ffc54b" strokeWidth="3" strokeDasharray="179.61224975585938 76.97667846679687">
+            <animate attributeName="stroke-dashoffset" calcMode="linear" values="0;256.58892822265625" keyTimes="0;1" dur="2" begin="0s" repeatCount="indefinite" />
+          </path>
+        </svg>
       :
         <form onSubmit={this.submitForm} className={this.props.className}>
           <input
